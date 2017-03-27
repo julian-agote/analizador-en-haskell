cb308ce72861f89ba347cc5fae7236428154a52c
julian-agote <julian.agote@gmail.com>
2017-03-27 17:47:47 +0200

    Add files via upload

--- prueba_parser_dc.hs
+++ prueba_parser_dc.hs
@@ -31,16 +31,23 @@ sigToken [] = [("$","")]
 sigToken x = (buscaSigToken (trim x) ""):(sigToken (substr (trim x) ((posb (snd (buscaSigToken (trim x) "")) 1 1 x)+(length (snd (buscaSigToken (trim x) "")))) (length x)))
 
 evaluar::Arbolsintactico->String
-evaluar x |((obtener_regla_aplicable x)=="DC -> OBJETO MENSAJE OBJETO nl DC")=(obtener_regla_aplicable x)++[chr(10)]++concat [(++) [chr(10)] (evaluar y)|y <- (devolver_ramas x)] 
-          |((obtener_regla_aplicable x)=="OBJETO->NOMBRE_OB CLASE")=(obtener_regla_aplicable x)++[chr(10)]++concat [(++) [chr(10)] (evaluar y)|y <- (devolver_ramas x)] 
-          |((obtener_regla_aplicable x)=="MENSAJE -> num : ITER id PARAMETROS CONDICION OPERACION")=(obtener_regla_aplicable x)++[chr(10)]++concat [(++) [chr(10)] (evaluar y)|y <- (devolver_ramas x)] 
-          |((obtener_regla_aplicable x)=="PARAMETROS->( LISTA )")=(obtener_regla_aplicable x)++[chr(10)]++(evaluar (head (tail(devolver_ramas x))))
+evaluar x |((obtener_regla_aplicable x)=="DC -> OBJETO MENSAJE OBJETO RDC")=(obtener_regla_aplicable x)++[chr(10)]++concat [(++) [chr(10)] (evaluar y)|y <- (devolver_ramas x)] 
+          |((obtener_regla_aplicable x)=="RDC -> punto_coma OBJETO MENSAJE OBJETO RDC")=(obtener_regla_aplicable x)++[chr(10)]++concat [(++) [chr(10)] (evaluar y)|y <- (devolver_ramas x)] 
+          |((obtener_regla_aplicable x)=="OBJETO -> NOMBRE_OB CLASE")=(obtener_regla_aplicable x)++[chr(10)]++concat [(++) [chr(10)] (evaluar y)|y <- (devolver_ramas x)] 
+          |((obtener_regla_aplicable x)=="MENSAJE -> num dos_puntos ITER id PARAMETROS")=(obtener_regla_aplicable x)++[chr(10)]++concat [(++) [chr(10)] (evaluar y)|y <- (devolver_ramas x)] 
+          |((obtener_regla_aplicable x)=="PARAMETROS-> par_ab LISTA par_ce")=(obtener_regla_aplicable x)++[chr(10)]++(evaluar (head (tail(devolver_ramas x))))
           |((obtener_regla_aplicable x)=="NOMBRE_OB -> id")=(obtener_regla_aplicable x)++[chr(10)]++(evaluar  (head (devolver_ramas x)))
-          |((obtener_regla_aplicable x)=="CLASE -> : id ")=(obtener_regla_aplicable x)++[chr(10)]++(evaluar (head (tail(devolver_ramas x))))
+          |((obtener_regla_aplicable x)=="CLASE -> dos_puntos id ")=(obtener_regla_aplicable x)++[chr(10)]++(evaluar (head (tail(devolver_ramas x))))
           |((obtener_regla_aplicable x)=="LISTA -> id RLISTA")=(obtener_regla_aplicable x)++[chr(10)]++concat [(++) [chr(10)] (evaluar y)|y <- (devolver_ramas x)] 
-          |((obtener_regla_aplicable x)=="RLISTA -> , id RLISTA")=(obtener_regla_aplicable x)++[chr(10)]++concat [(++) [chr(10)] (evaluar y)|y <- (tail (devolver_ramas x))] 
+          |((obtener_regla_aplicable x)=="RLISTA -> coma id RLISTA")=(obtener_regla_aplicable x)++[chr(10)]++concat [(++) [chr(10)] (evaluar y)|y <- (tail (devolver_ramas x))] 
           |otherwise="otro="++(obtener_regla_aplicable x)++[chr(10)]
 
+obtener_las_clases::Arbolsintactico->String
+obtener_las_clases x |((obtener_regla_aplicable x)=="CLASE -> dos_puntos id ")=devolver_id (head(tail (devolver_ramas x)))
+                     |((obtener_regla_aplicable x)=="DC -> OBJETO MENSAJE OBJETO RDC")=concat [(++) "," (obtener_las_clases y)|y <- (devolver_ramas x)] 
+					 |((obtener_regla_aplicable x)=="RDC -> punto_coma OBJETO MENSAJE OBJETO RDC")=concat [(++) "," (obtener_las_clases y)|y <- (tail(devolver_ramas x))]
+					 |((obtener_regla_aplicable x)=="OBJETO -> NOMBRE_OB CLASE")=concat [(++) "," (obtener_las_clases y)|y <- (devolver_ramas x)] 					 
+					 |otherwise=[]
 
 main:: IO ()
 main = do
@@ -48,5 +55,6 @@ main = do
         y <- hGetContents h1
         -- if (instrb (parser_slr (sigToken y) [1]) "entrada correcta" 1 1)>0 then putStrLn (evaluar (parser_slr_arbol (sigToken y) [1] [])) 
                                                                            -- else putStrLn ((parser_slr (sigToken y) [1]))  
-        putStrLn ((parser_slr (sigToken y) [1]))
+        --putStrLn ((parser_slr (sigToken y) [1]))
+		putStrLn (obtener_las_clases((parser_slr_arbol (sigToken y) [1] [])))
         hClose h1       
